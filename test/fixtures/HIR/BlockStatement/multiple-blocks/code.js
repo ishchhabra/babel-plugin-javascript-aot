b89fcb5c8216ccacc54e0a9ad41c5975085c5754
Ish Chhabra <ishchhabra12@gmail.com>
2024-12-20 18:12:10 -0600

    Add test for BlockStatement

---
+++ test/fixtures/HIR/BlockStatement/multiple-blocks/code.js
@@ -0,0 +1,9 @@
+{
+  const value = 1;
+  console.log(value);
+}
+
+{
+  const value = 2;
+  console.log(value);
+}
---
+++ test/fixtures/HIR/BlockStatement/multiple-blocks/output.js
@@ -0,0 +1,6 @@
+const $1 = 1;
+const $0 = $1;
+console.log($0);
+const $4 = 2;
+const $3 = $4;
+console.log($3);
