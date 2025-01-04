f86bbe7ccdf0944dc7c0e1780bfeb0b6081a4482
Ish Chhabra <ishchhabra12@gmail.com>
2025-01-03 20:05:18 -0600

    Remove .eslintignore file and update eslint.config.js to ignore the dist/ directory

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import tsParser from "@typescript-eslint/parser";
 export default [
   {
     files: ["src/**/*.ts", "test/**/*.ts"],
+    ignores: ["dist/**"],
     plugins: {
       "@typescript-eslint": eslint,
     },
