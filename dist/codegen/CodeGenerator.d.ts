ebb79563e78a7229ebc92609d165f4916acb460e
GitHub Action <action@github.com>
2025-01-06 18:29:59 +0000

    Create Release: v0.0.13

---
+++ dist/codegen/CodeGenerator.d.ts
@@ -0,0 +1,15 @@
+import * as t from "@babel/types";
+import { BasicBlock, BlockId } from "../ir";
+/**
+ * Generates the code from the IR.
+ */
+export declare class CodeGenerator {
+    #private;
+    private readonly blocks;
+    private readonly backEdges;
+    private readonly places;
+    private readonly blockToStatements;
+    private readonly generatedBlocks;
+    constructor(blocks: Map<BlockId, BasicBlock>, backEdges: Map<BlockId, Set<BlockId>>);
+    generate(): t.Program;
+}
